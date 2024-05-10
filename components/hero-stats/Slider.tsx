23b1695bf814dba53f9033b611e940d41e3fb8dc
exyted1 <111304641+exyted1@users.noreply.github.com>
2024-05-10 13:45:10 +0700

    update banner

--- components/hero-stats/Slider.tsx
+++ components/hero-stats/Slider.tsx
@@ -2,48 +2,48 @@ import React, { useEffect, useRef, useState } from 'react';
 
 const images: { desktop: string, mobile: string }[] = [
   {
-    desktop: "https://i.ibb.co/QJbfKTt/20240320-004858-0000.png",
-    mobile: "https://i.ibb.co/QJbfKTt/20240320-004858-0000.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/0.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/0.png"
   },
   {
-    desktop: "https://i.ibb.co/5sfrWgv/20240320-004859-0001.png",
-    mobile: "https://i.ibb.co/5sfrWgv/20240320-004859-0001.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/1.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/1.png"
   },
   {
-    desktop: "https://i.ibb.co/PCS7NWk/20240320-004859-0002.png",
-    mobile: "https://i.ibb.co/PCS7NWk/20240320-004859-0002.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/2.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/2.png"
   },
   {
-    desktop: "https://i.ibb.co/sbFhNS6/20240320-004859-0003.png",
-    mobile: "https://i.ibb.co/sbFhNS6/20240320-004859-0003.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/3.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/3.png"
   },
   {
-    desktop: "https://i.ibb.co/sR4qydN/20240320-004859-0004.png",
-    mobile: "https://i.ibb.co/sR4qydN/20240320-004859-0004.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/4.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/4.png"
   },
   {
-    desktop: "https://i.ibb.co/TWVq5st/20240320-004859-0005.png",
-    mobile: "https://i.ibb.co/TWVq5st/20240320-004859-0005.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/5.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/5.png"
   },
   {
-    desktop: "https://i.ibb.co/XL0X14b/20240320-004859-0006.png",
-    mobile: "https://i.ibb.co/XL0X14b/20240320-004859-0006.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/6.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/6.png"
   },
   {
-    desktop: "https://i.ibb.co/3rMLQDF/20240320-004859-0007.png",
-    mobile: "https://i.ibb.co/3rMLQDF/20240320-004859-0007.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/7.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/7.png"
   },
   {
-    desktop: "https://i.ibb.co/TPnx2Fj/20240320-004859-0008.png",
-    mobile: "https://i.ibb.co/TPnx2Fj/20240320-004859-0008.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/8.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/8.png"
   },
   {
-    desktop: "https://i.ibb.co/v1PTmVR/20240320-004859-0009.png",
-    mobile: "https://i.ibb.co/v1PTmVR/20240320-004859-0009.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/9.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/9.png"
   },
   {
-    desktop: "https://i.ibb.co/ZTzDZBJ/20240320-004859-0011.png",
-    mobile: "https://i.ibb.co/ZTzDZBJ/20240320-004859-0011.png"
+    desktop: "https://raw.githubusercontent.com/exyted1/assets/main/banner/10.png",
+    mobile: "https://raw.githubusercontent.com/exyted1/assets/main/banner/10.png"
   }
 ];
 const delay = 4000;
